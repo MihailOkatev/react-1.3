80bc7b9d2e3e18af2a8d3ca89f11740ae7b4107b
AppVeyor <ci@appveyor.com>
2020-09-19 14:42:33 +0000

    Updates

---
+++ precache-manifest.e0e66fe4f067f74a6c815f5c3b211f65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c0d1b0ea1fd856dddb9bb196c090dc",
+    "url": "/MihailOkatev/react-1.3/index.html"
+  },
+  {
+    "revision": "3f544879850b2d6c4599",
+    "url": "/MihailOkatev/react-1.3/static/css/main.2010cc3d.chunk.css"
+  },
+  {
+    "revision": "abe64b723257d44075a7",
+    "url": "/MihailOkatev/react-1.3/static/js/2.0dbdcc4e.chunk.js"
+  },
+  {
+    "revision": "590b288afc476ced67e9258cf268eacb",
+    "url": "/MihailOkatev/react-1.3/static/js/2.0dbdcc4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f544879850b2d6c4599",
+    "url": "/MihailOkatev/react-1.3/static/js/main.c0b19d0c.chunk.js"
+  },
+  {
+    "revision": "45021218c81f45e885a4",
+    "url": "/MihailOkatev/react-1.3/static/js/runtime-main.cbafafc6.js"
+  }
+]);
